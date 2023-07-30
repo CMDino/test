81a8953da0186c3c080bfaafacb8815ff5b88bf7
GPultrone <giuseppe.pul7@gmail.com>
2023-07-30 18:21:15 +0200

    aaaa

--- tools/auto_git/main.py
+++ tools/auto_git/main.py
@@ -11,17 +11,12 @@ def run_git_command(command, data):
       if "CONFLICT (content): Merge conflict" in e.stdout:
          while True:
             try:
-               print("riga 18")
                result = subprocess.run(["git", "merge", data["branch"]], cwd=data["path"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True, text=True)
-               print(f"riga 20 {result.stdout}")
                break
             except subprocess.CalledProcessError as e:
-               print(f"riga 23 {e.stdout} ----- {e.stderr}")
                if "(MERGE_HEAD exists)" in e.stderr:
                   result = subprocess.run(["git", "commit", "-m", "test"], cwd=data["path"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True, text=True)
                   break
-      input()
-      #sys.exit(1)
 
 def main():
    with open("configuration.json", "r") as file:
@@ -37,13 +32,6 @@ def main():
    run_git_command(["git", "checkout", data["branch"]], data)
    run_git_command(["git", "merge", "develop"], data)
    run_git_command(["git", "push"], data)
-
-# CIAAAAOOOOO
-# delta
-# delta
-# delta
-# delta
-# CIIAOOOOOOO
 
 if __name__ == "__main__":
    main()
