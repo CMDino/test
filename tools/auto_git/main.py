ef46044137704d9c952154b82d31c77215668ec6
GPultrone <giuseppe.pul7@gmail.com>
2023-07-30 15:02:03 +0200

    "test46"

--- tools/auto_git/main.py
+++ tools/auto_git/main.py
@@ -38,7 +38,7 @@ def main():
    run_git_command(["git", "push"], data)
    run_git_command(["git", "checkout", data["branch"]], data)
 
-# CIAO
+# suca
 # suca
 # CIIIIAAAAAAOOOO
 # CIAIAOIAOAIOIAOA
